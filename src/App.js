022b5fe085b0ff0e4b5ba1d2537c2d59f9c3cd36
Dillon Donoho <scopicaudio@gmail.com>
2022-05-23 20:08:58 -0400

    fixed particle system

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './42Recordings_Logo_Black+Transparent-01.png';
 import './App.css';
+import Particles from './components/Particles';
 
 function App() {
   return (
-    <>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-    </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://particles.js.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          tsParticles
+        </a> */}
+      </header>
+      <Particles id="tsparticles" />
+    </div>
   );
 }
 
 export default App;
-
