5172b7ad8838f1214e6fe7c0c5793f2a5fa12699
Dillon Donoho <dillondonoho@Dillons-MacBook-Pro.local>
2022-06-13 16:42:57 -0400

    added artist images

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,16 @@
 // import Particles from '../components/Particles';
 import Navbar from "../components/NavBar";
 import React from "react";
+import ArtistCards from "../components/ArtistCards";
+import Particles from '../components/Particles';
 
 function Home() {
   return (
-    <div className="App">
+    <div className="Home">
       <Navbar />
       Home Page
+      <ArtistCards />
+      <Particles id="tsparticles" />
     </div>
   );
 }
