eb791ffc08d26e22ff6fc333b6332699122eb2b5
donohodillon <scopicaudio@gmail.com>
2022-06-21 14:17:14 -0400

    started booking form

--- src/components/BookingsForm.js
+++ src/components/BookingsForm.js
@@ -4,21 +4,20 @@ import "./BookingsForm.css";
 const BookingsForm = () => {
   return (
     <div className="bookings-form">                  
-      <form>
-        <label>
-            Artist Name:
-            <input type="text" name="name" />
-        </label>
-        <label>
-            Artist Name:
-            <input type="text" name="name" />
-        </label>
-        <label>
-            Artist Name:
-            <input type="text" name="name" />
-        </label>
-            <input type="submit" value="Submit" />
+      <body>
+    <form action="/" class="decor">
+      <div class="form-left-decoration"></div>
+      <div class="form-right-decoration"></div>
+      <div class="circle"></div>
+      <div class="form-inner">
+        <h1>Request booking</h1>
+        <input type="text" placeholder="Artist"/>
+        <input type="email" placeholder="Email"/>
+        <textarea placeholder="Message..." rows="5"></textarea>
+        <button type="submit-booking" href="/">Submit</button>
+      </div>
     </form>
+  </body>
     </div>
   );
 };
